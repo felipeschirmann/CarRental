dc5b2af6555ae2b668d53b8da140207ce5386fb0
Felipe Schirmann <contato@felipeschirmann.dev.br>
2023-01-30 17:48:44 -0300

    refactor: main application format

--- src/application/main.java
+++ src/application/main.java
@@ -18,10 +18,9 @@ public class main {
 		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Entre com os dados do aluguel");
+		System.out.println("Entre com os dados do aluguel");
 		System.out.print("Modelo do carro:");
 		String carModel = sc.nextLine();
-		System.out.println();
 		System.out.print("Retirada (dd/MM/yyyy hh:mm):");
 		LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
 		System.out.print("Retorno (dd/MM/yyyy hh:mm):");
@@ -29,9 +28,9 @@ public class main {
 
 		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
 
-		System.out.println("Entre com o preço por hora:");
+		System.out.print("Entre com o preço por hora:");
 		Double pricePerHour = sc.nextDouble();
-		System.out.println("Entre com o preço por dia:");
+		System.out.print("Entre com o preço por dia:");
 		Double pricePerDay = sc.nextDouble();
 
 		RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
