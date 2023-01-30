47d566b3985f99ec6be4758e9b5b73fca44e3ed6
Felipe Schirmann <contato@felipeschirmann.dev.br>
2023-01-30 17:36:20 -0300

    refactor: RentalService calc of basicPayment and tax

--- src/services/RentalService.java
+++ src/services/RentalService.java
@@ -1,21 +1,36 @@
 package services;
 
+import java.time.Duration;
+
 import entities.CarRental;
 import entities.Invoice;
 
 public class RentalService {
 	private Double pricePerHour;
-	private Double pricePerday;
+	private Double pricePerDay;
 
 	private BrazilTaxService taxService;
 
 	public RentalService(Double pricePerHour, Double pricePerday, BrazilTaxService taxService) {
 		this.pricePerHour = pricePerHour;
-		this.pricePerday = pricePerday;
+		this.pricePerDay = pricePerday;
 		this.taxService = taxService;
 	}
 
 	public void processInvoice(CarRental carRental) {
-		carRental.setInvoice(new Invoice(50.0 , 10.0));
+
+		double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+		double hours = minutes / 60.0;
+
+		double basicPayment;
+		if (hours <= 12.0) {
+			basicPayment = pricePerHour * Math.ceil(hours);
+		} else {
+			basicPayment = pricePerDay * Math.ceil(hours / 24);
+		}
+		
+		double tax = taxService.tax(basicPayment);
+
+		carRental.setInvoice(new Invoice(basicPayment, tax));
 	}
 }
